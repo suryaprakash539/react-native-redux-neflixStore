d48aab8f88f5a45fbdfc047b72673a5800315239
suryaprakash <surya.zvky@gmail.com>
2020-11-30 13:54:48 +0530

    made store available to components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {StatusBar} from 'react-native';
+// for react navigation
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
+//screens
+import Home from './screens/Home';
+import Add from './screens/Add';
+
+import {Provider} from 'react-redux';
+import store from './store';
+
+const Stack = createStackNavigator();
+
+// setting the header as default in all screen
 const App = () => {
-  return <Text>Redux App</Text>;
+  return (
+    <Provider store={store}>
+      <StatusBar backgroundColor="#0f4c75" />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#0f4c75',
+            },
+            title: 'LCO Netflix App',
+            headerTitleStyle: {
+              textAlign: 'center',
+              color: '#00b7c2',
+            },
+          }}>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Add" component={Add} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
 };
 
 export default App;
---
+++ src/screens/Add.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Add = () => {
+  return <Text>Add</Text>;
+};
+
+export default Add;
